7f53b872467ccc55fb698a9ec03e93653ae05d8c
amirbous <a.amirbouslama@gmail.com>
2022-03-16 16:20:16 +0100

    new program: inverse binary matrix

---
+++ inverse_binary_matrix.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void fill_row () {
+
+}
+
+int to_int (const char *s) {
+    return atoi(s);
+}
+
+int main(int argc, char *argv[]) {
+    int row;
+    int height;
+    for (int i = 0; i < to_int(argv[3]); i++) {
+
+        scanf();
+
+    }
+
+}
