24e760af9c1ca87a5895a53037f05da3c3d5f23b
amirbous <a.amirbouslama@gmail.com>
2022-03-17 12:33:34 +0100

    data input and filling out the matrix

--- inverse_binary_matrix.c
+++ inverse_binary_matrix.c
@@ -1,21 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void fill_row () {
 
-}
 
 int to_int (const char *s) {
     return atoi(s);
 }
+void fill_matrix (int height, int width, int result[height][width]) {
 
-int main(int argc, char *argv[]) {
-    int row;
-    int height;
-    for (int i = 0; i < to_int(argv[3]); i++) {
-
-        scanf();
+    for (int i = 0; i < height; i++) {
+        char row[width];
+        int a ;
+        printf("row n%d: ", i);
+        scanf("%s", row);
+        for (int j = 0; j < width;j++) result[i][j] = to_int(row[j]);
 
     }
+}
+void invert_matrix(int height, int width, int result[height][width]){
+
+
+
+
+}
+int main(int argc, char *argv[]) {
+    int width = to_int(argv[1]);
+    int height = to_int(argv[2]);
+    int matrix[height][width];
+    fill_matrix(height, width, matrix);
 
 }
