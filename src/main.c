315f264bff5110d0ed8f75cd3c3727d1810d87c4
Amir Bouslama <a.amirbouslama@gmail.com>
2023-11-26 18:48:46 +0100

    cleaner console for buffering stdin, exit command

--- src/main.c
+++ src/main.c
@@ -1,22 +1,28 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 
 #define BUFFER_SIZE 1024
 
 int main(){
-	char *buffer = malloc(BUFFER_SIZE);
+	char* buffer = malloc(BUFFER_SIZE);
+	char* command;
 	int i;
-	while(fgets(buffer, sizeof buffer, stdin) != NULL) {  // reads newline too
-        	i = 0;
-        	while (buffer[i] != '\0') {                     // shortcut to testing newline and nul
-            	printf("%d ", buffer[i]);                  // print char value
-            	i++;
-        	}
-        printf ("\n");
-        free(buffer);     	
-	}
-
+    	do {
+        	printf("> "); /* (A) */
+        	scanf("%s", buffer); /* (B) */
+		i = 0;
+		command = malloc(strlen(buffer)* sizeof(char));
+		strcpy(command, buffer);
+		if (strcmp(command, "exit") == 0) return 0;
+		while (command[i] != '\0') {
+			printf("%c", buffer[i]);	
+    			i++;
+		}
+		printf("\n");
+	} while(1);
+    	return 0;
 	  
 return 0;
 
